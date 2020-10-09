78247f18c9f93d31ec91ca6cc85bb2b0b4486ded
Jimmy Mendy <jimmy.mendy@gmail.com>
2020-10-09 12:21:48 +0100

    First React website

---
+++ src/components/ServicesSection/Data.js
@@ -0,0 +1,18 @@
+export const ServiceOne = {
+  id: 'One',
+  img: require('../../images/svg-1.svg'),
+  h2: 'Reduce expense',
+  p: 'We help reduce your fees and increase your overall revenue.',
+};
+export const ServiceTwo = {
+  id: 'Two',
+  img: require('../../images/svg-1.svg'),
+  h2: 'Virtual Offices',
+  p: 'You can access our platform online anywhere in the world.',
+};
+export const ServiceThree = {
+  id: 'Three',
+  img: require('../../images/svg-1.svg'),
+  h2: 'Premium Benefit',
+  p: 'Unlock our special membership card that returns 5% cash back.',
+};
